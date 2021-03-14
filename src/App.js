732458640f2af8ee8ac5cd6b910cfec452df516b
Natka <natalia.kur@yahoo.com>
2021-03-14 20:08:43 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Natalia Kur and is{" "}
-        <a
-          href="https://github.com/Katalia91/react-weather-app"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Natalia Kur and is{" "}
+          <a
+            href="https://github.com/Katalia91/react-weather-app"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
