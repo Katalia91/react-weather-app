db949fe7f86564042405b3a9905820115f0026f4
Natka <natalia.kur@yahoo.com>
2021-04-06 20:38:53 +0200

    Corrected an error with background change

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,6 @@ import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
-import ChangeBackground from "./ChangeBackground";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
